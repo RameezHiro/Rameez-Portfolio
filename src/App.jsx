e75e83b995b2d6010ae2ab57e1a4c88199687f48
RameezHiro <shaikhrameeznayab@gmail.com>
2026-04-05 21:25:14 +0530

    Resolved Merge Conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Analytics } from '@vercel/analytics/react';
 
 const TopNavBar = () => {
   const [activeSection, setActiveSection] = React.useState('');
